76cd55e89eda2c7a1f1f9b180caea79f31b733af
Jorge Castello <jorgio.castello@gmail.com>
2020-04-21 12:31:31 -0700

    Set up form + light CSS styling

--- server/server.js
+++ server/server.js
@@ -9,4 +9,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '../client')));
 
+app.get('/', (req, res) => {
+  res.send('Goodybe World');
+});
+
+app.post('/createCSV', (req, res) => {
+  console.log(JSON.parse(req.body));
+  res.send('YO');
+});
+
 app.listen(3000, () => console.log('Server is up and running on Port 3000...'));
