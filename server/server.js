39de378eca09c3f55ba402a7917ce15debd27307
Jorge Castello <jorgio.castello@gmail.com>
2020-04-21 11:47:20 -0700

    Install dependencies and create server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.listen(3000, () => console.log('Server is up and running on Port 3000...'));
